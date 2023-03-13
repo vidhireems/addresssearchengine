f386e7ad5409d698b5b37cdab2d10bee609490e7
vidhireems <vidhirrathod@gmail.com>
2023-03-12 22:34:40 -0700

    Updating model to package

---
+++ backend/model/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='model',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # list the package's dependencies here
+    ]
+)
